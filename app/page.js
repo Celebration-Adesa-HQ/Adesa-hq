7956d815b92ecf1d4edb38c626685b9cdeb84450
Celebration-cloud <253773781+Celebration-Adesa-HQ@users.noreply.github.com>
2026-02-20 14:16:50 +0100

    fix: `Added metadata and updated Home component`

--- app/page.js
+++ app/page.js
@@ -1,7 +1,14 @@
 import Maintenance from "@/components/Maintenance";
 
+export const metadata = {
+  title: "Adesa HQ | Under Maintenance",
+  description:
+    "Adesa HQ is currently undergoing scheduled maintenance. We will be back shortly.",
+  alternates: {
+    canonical: "https://www.adesahq.com/",
+  },
+};
+
 export default function Home() {
-  return (
-    <Maintenance />
-  );
+  return <Maintenance />;
 }
