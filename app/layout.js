cf54c8518dd97f32fdfab7aeff7c5840424dc234
Celebration-cloud <253773781+Celebration-Adesa-HQ@users.noreply.github.com>
2026-02-20 14:08:53 +0100

    fix: `Added Google Bot metadata to robots configuration`

--- app/layout.js
+++ app/layout.js
@@ -54,6 +54,18 @@ export const metadata = {
   robots: {
     index: true,
     follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+
+  icons: {
+    icon: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
   },
 };
 
