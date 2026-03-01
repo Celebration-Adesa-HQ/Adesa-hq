034805bd34a4da4e56be2564290df4079e68a229
Celebration-cloud <253773781+Celebration-Adesa-HQ@users.noreply.github.com>
2026-03-01 06:35:58 +0100

    `Update siteConfig URL to https://adesahq.com`

--- config/site.jsx
+++ config/site.jsx
@@ -2,7 +2,7 @@ export const siteConfig = {
   name: "ADESA HQ",
   description:
     "ADESA HQ is a builder-operator group focused on conceiving, launching, and scaling sector-defining African businesses.",
-  url: "https://www.adesahq.com",
+  url: "https://adesahq.com",
   ogImage: "/logo/Adesa-HQ.-logo.png",
   links: {
     twitter: "https://twitter.com/adesahq",
